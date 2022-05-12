20db90f655d459c9c7083281d9b277d0848e0030
Osvaldo <osvruizgar@gmail.com>
2022-05-11 21:08:15 -0400

    finished

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //easy
-// function average(array) {
-//   let total = 0;
-//   for (let arrayLoop = 0; arrayLoop < array.length; arrayLoop++) {
-//     total = total + array[arrayLoop];
-//   }
-//   return total / array.length;
-// }
+function average(array) {
+  let total = 0;
+  for (let arrayLoop = 0; arrayLoop < array.length; arrayLoop++) {
+    total = total + array[arrayLoop];
+  }
+  return total / array.length;
+}
 // console.log(average([1, 4, 7]));
 // console.log(average([10, 5]));
 // console.log(average([1.5, 3, 2.5, 1]));
@@ -26,5 +26,21 @@ function findNumber(num) {
   return arrays.indexOf(num);
 }
 console.log(findNumber(2));
-//hard
-window.updateTheBackground = function (className) {};
+//very hard
+function change(coin, total) {
+  const newArray = new Array(total + 1).fill(Infinity);
+  newArray[0] = 0;
+  for (let c of coin) {
+    for (let i = 0; i < newArray.length; i++) {
+      if (c <= i) {
+        let index = i - c;
+        let amount = newArray[index] + 1;
+        newArray[i] = Math.min(amount, newArray[i]);
+      }
+    }
+  }
+  return newArray[newArray.length - 1] === Infinity
+    ? -1
+    : newArray[newArray.length - 1];
+}
+console.log(change([1, 2, 5], 11));
